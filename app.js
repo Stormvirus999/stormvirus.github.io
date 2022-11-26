1fefac4be935c5265ff324676141db6d517ca00e
Stormvirus999 <78990185+Stormvirus999@users.noreply.github.com>
2022-11-26 15:51:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,21 +16,6 @@ function opentab(tabname)
 }
 
 
-/*function sendEmail()
-{
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "vedantlonkar2004@gmail.com",
-        Password : "DIGITALpirate@23",
-        To : 'lvedant99@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "new contact form enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message) 
-    );
-    
-}*/
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbykcwPb1X4PehB5WrNatBJAjPLbyX_zfnDsItQWlRQqazZZJ_BCtdhFyYacXNQJCfhr/exec'
 const form = document.forms['submit-to-google-sheet']
@@ -40,4 +25,6 @@ form.addEventListener('submit', e => {
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => console.log('Success!', response))
     .catch(error => console.error('Error!', error.message))
+        window.alert("Thanks for your query we will response ASAP!!!");
+
 })
