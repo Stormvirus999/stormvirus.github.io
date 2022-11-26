bdeefa61c44baabf01a72ee28f92e1f757e6ea18
Stormvirus999 <78990185+Stormvirus999@users.noreply.github.com>
2022-11-26 16:00:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,14 +17,15 @@ function opentab(tabname)
 
 
 
+
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbykcwPb1X4PehB5WrNatBJAjPLbyX_zfnDsItQWlRQqazZZJ_BCtdhFyYacXNQJCfhr/exec'
 const form = document.forms['submit-to-google-sheet']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    .then(response => console.log('Success!',response()))
     .catch(error => console.error('Error!', error.message))
-        window.alert("Thanks for your query we will response ASAP!!!");
-
+    window.alert("Thanks for your query we will response ASAP!!!");
 })
